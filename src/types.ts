ff14bc40f50b326d0a9ef11af1a8e76b3711b7a2
borjomeeee <spiridonov.new@gmail.com>
2022-05-25 13:28:27 +0300

    promisify services

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import type { Metadata, CallOptions } from '@grpc/grpc-js';
+
 export type { ProtoGrpcType as InstrumentsType } from './generated/instruments';
 export type { ProtoGrpcType as MarketdataType } from './generated/marketdata';
 export type { ProtoGrpcType as OrdersType } from './generated/orders';
@@ -6,8 +8,8 @@ export type { ProtoGrpcType as SandboxType } from './generated/sandbox';
 export type { ProtoGrpcType as StopordersType } from './generated/stoporders';
 export type { ProtoGrpcType as UsersType } from './generated/users';
 
-export type Client<Constructor> =  Constructor extends (new (...args: any) => infer R) ? R : null;
-export type ServicePath<T extends Basic> = T['tinkoff']['public']['invest']['api']['contract']['v1'];
+export type ServicePath<T extends Basic> =
+  T['tinkoff']['public']['invest']['api']['contract']['v1'];
 export type Basic = {
   tinkoff: {
     public: {
@@ -21,3 +23,20 @@ export type Basic = {
     };
   };
 };
+
+type NotUndef<T> = T extends undefined ? never : T;
+type PromiseMethod<T> = T extends (args: infer ARGS, cb: () => any) => any
+  ? (
+      args: ARGS,
+      metadata?: Metadata,
+      options?: CallOptions
+    ) => Promise<NotUndef<Parameters<Parameters<T>[1]>[1]>>
+  : never;
+
+export type InputService = { [key: string]: any };
+export type PromiseService<Service> = Service extends InputService
+  ? { [Method in keyof Service]: PromiseMethod<Service[Method]> }
+  : never;
+
+export type Client<Constructor> = Constructor extends new (...args: any) => infer R ? R : null;
+export type PromiseClient<T> = PromiseService<Client<T>>;
